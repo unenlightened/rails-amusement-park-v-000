f4d08e841276088fb2d6428d6195ae63239a7a7a
Chely Ho <adept_inept@yahoo.com>
2018-04-27 06:03:52 +0000

    automatically backed up by learn

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -30,11 +30,11 @@ class AttractionsController < ApplicationController
   end
 
 #can put this in a ride controller as ride create ....
-  # def ride
-  #   ride = Ride.create(user_id: current_user.id, attraction_id: params[:id])
-  #   ride_message = ride.take_ride
-  #   redirect_to ride.user, alert: ride_message
-  # end
+  def ride
+    ride = Ride.create(user_id: current_user.id, attraction_id: params[:id])
+    ride_message = ride.take_ride
+    redirect_to ride.user, alert: ride_message
+  end
 
   private
 
