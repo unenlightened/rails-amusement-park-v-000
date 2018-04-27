c4426ea6d96bd7f2138013f9d95dc8fe7cfc449c
Chely Ho <adept_inept@yahoo.com>
2018-04-27 06:08:06 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,9 @@ Rails.application.routes.draw do
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
-  post 'rides/new' => 'rides#new'
-
   resources :users, only: [:new, :create, :show, :edit]
   resources :attractions, only: [:index, :new, :create, :show, :edit, :update]
 
-  # post '/attractions/:id/ride' => 'attractions#ride', as: 'ride_attraction'
+  post '/attractions/:id/ride' => 'attractions#ride', as: 'ride_attraction'
 
 end
