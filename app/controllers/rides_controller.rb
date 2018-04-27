72d8480b6f171aceef954ffa8de3efe0ccc3c268
Chely Ho <adept_inept@yahoo.com>
2018-04-27 05:54:20 +0000

    automatically backed up by learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
-  def index
+  def ride
+    ride = Ride.create(user_id: current_user.id, attraction_id: params[:id])
+    ride_message = ride.take_ride
+    redirect_to ride.user, alert: ride_message
   end
 end
